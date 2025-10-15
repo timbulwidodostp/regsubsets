62fd0f19807e3f06dce536fcd96a718bae1fc491
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-10-15 10:11:29 +0700

    regsubsets (leaps) With (In) R Software

---
+++ regsubsets/regsubsets.R
@@ -0,0 +1,15 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Functions for model selection Use regsubsets (leaps) With (In) R Software
+install.packages("leaps")
+library("leaps")
+npregbw = read.csv("https://raw.githubusercontent.com/timbulwidodostp/npregbw/main/npregbw/npregbw.csv",sep = ";")
+# Estimation Functions for model selection Use regsubsets (leaps) With (In) R Software
+regsubsets <- regsubsets(Fertility ~ Agriculture + Examination + Education + Catholic + Infant.Mortality, data = regsubsets)
+summary(regsubsets)
+# Functions for model selection Use regsubsets (leaps) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
